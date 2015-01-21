b36ccf414c05a5cfccea364c5d80ebdd67e0e809
Frederic Bonifas <fredericbonifas@gmail.com>
2015-01-21 18:12:23 +0100

    Issue #4: start from any SVG added in the settings

--- app/scripts/controllers/globalmap.js
+++ app/scripts/controllers/globalmap.js
@@ -11,15 +11,38 @@ angular.module('accessimapEditeurDerApp')
   .controller('GlobalmapCtrl', ['$scope', '$http', 'usSpinnerService', 'initSvg', 'mapService', 'settings', 'exportService',
     function ($scope, $http, usSpinnerService, initSvg, mapService, settings, exportService) {
 
+      $scope.mapCategories = [{
+        id: 'world',
+        name: 'Monde',
+        images: [{
+          path: 'data/BlankMap-World6-Equirectangular.svg'
+        }]
+      }, {
+        id: 'france',
+        name: 'France',
+        images: [{
+          path: 'data/France_all_regions_A4.svg'
+        }]
+      }];
+
       var width = 1000,
           legendWidth = 300,
           height = width / Math.sqrt(2),
           margin = 10;
 
-      var svg = d3.xml("data/France_all_regions_A4.svg", function(xml) {
-        d3.select("#map").node().appendChild(xml.documentElement);
-      });
+      function appendSvg(path) {
+        $scope.accordionStyle = {display: "none"};
+        var svg = d3.xml(path, function(xml) {
+          d3.select("#map")
+            .selectAll("svg")
+            .remove();
+          d3.select("#map")
+            .node()
+            .appendChild(xml.documentElement);
+        });
+      }
 
       $scope.mapExport = exportService.mapExport;
+      $scope.appendSvg = appendSvg;
 
 }]);
