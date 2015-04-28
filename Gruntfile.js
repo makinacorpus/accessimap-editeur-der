5d8934257e69d38645f9d44bf68561333079d98a
Frederic Bonifas <fredericbonifas@gmail.com>
2015-04-28 17:13:22 +0200

    dist with fontawesome

--- Gruntfile.js
+++ Gruntfile.js
@@ -354,6 +354,11 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/fontawesome/fonts/',
+          src: '*',
+          dest: '<%= yeoman.dist %>/fonts'
         }]
       },
       styles: {
