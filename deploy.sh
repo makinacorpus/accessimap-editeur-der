7edacbec2d281709798aa37d19f95d53a7ee66b3
Mathieu DARTIGUES <mathieu@dartic.fr>
2016-11-28 16:59:40 +0100

    Moving git config at the begin of deploy.sj

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 # Enable error reporting to the console.
 set -e
 
+# Make sure we have the updated .travis.yml file so tests won't run on master.
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.name "Travis CI"
+
 # Checkout `master` and remove everything.
 git clone https://${GH_TOKEN}@github.com/makinacorpus/accessimap-editeur-der.git accessimap-editeur-der-gh-pages
 cd accessimap-editeur-der-gh-pages
@@ -12,10 +16,6 @@ git checkout gh-pages
 # Copy generated HTML site from source branch in original repository.
 # Now the `master` branch will contain only the contents of the _site directory.
 cp -R ../dist/* .
-
-# Make sure we have the updated .travis.yml file so tests won't run on master.
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-git config user.name "Travis CI"
 
 # Commit and push generated content to `master` branch.
 git status
